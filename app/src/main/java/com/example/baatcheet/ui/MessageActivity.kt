48b19b8c9bb748d3b05c918db66de3947b6ef573
Manish Panwar <panwarmanish843@gmail.com>
2020-11-18 12:11:05 +0530

    Added Custom Action Bar

--- app/src/main/java/com/example/baatcheet/ui/MessageActivity.kt
+++ app/src/main/java/com/example/baatcheet/ui/MessageActivity.kt
@@ -2,14 +2,33 @@ package com.example.baatcheet.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 import com.example.baatcheet.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MessageActivity : AppCompatActivity() {
+    companion object{
+        val db=FirebaseFirestore.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
-        actionBar?.setDisplayShowCustomEnabled(true)
-        actionBar?.setCustomView(R.layout.actionbar_message)
-        
+        val actionBar=supportActionBar
+        actionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+            setDisplayShowHomeEnabled(false)
+            setDisplayShowCustomEnabled(true)
+            setCustomView(R.layout.actionbar_message)
+        }
+        val name=findViewById<TextView>(R.id.name)
+        val id=intent.extras?.get("id")
+        db.collection("users").document("$id").addSnapshotListener { value, error ->
+            name.text=value?.get("name").toString()
+
+        }
+
     }
 }
